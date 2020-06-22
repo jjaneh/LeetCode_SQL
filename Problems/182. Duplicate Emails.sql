bf32e67ed66f6ea59e33500dd14a4cc07005aefc
jjaneh <56518116+jjaneh@users.noreply.github.com>
2020-06-22 09:36:06 +0800

    Update 182. Duplicate Emails.sql

--- Problems/182. Duplicate Emails.sql
+++ Problems/182. Duplicate Emails.sql
@@ -1,11 +1,11 @@
-#Solution 1
+-- Solution 1
 SELECT Email
 FROM (SELECT Email, count(Email) AS num
       FROM Person
       GROUP BY Email) AS statistic
 WHERE num > 1
 
-#Solution 2
+-- Solution 2
 SELECT Email
 FROM Person
 GROUP BY Email
